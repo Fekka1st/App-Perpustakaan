43fb4a4f4c200de65446ad8597582ed2ce1af7c5
Ferry Aditya H <ferryaditya124@gmail.com>
2022-11-20 19:56:57 +0700

    #fix 2 , error form update

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -41,7 +41,16 @@
                                         [gambar tidak tersedia]
                                     @endif
                                 </td>
-                                <td></td>
+                                <td>
+                                    <div class="btn-group" role="group" aria-label="Basic Example">
+                                        <button type="button" id="btn-edit-buku" class="btn btn-success"
+                                            data-toggle="modal" data-target="#editBukuModal"
+                                            data-id="{{ $book->id }}">Edit</button>
+                                        <button type="button" id="btn-hapus-buku" class="btn btn-danger"
+                                            data-toggle="modal" data-target="#hapusBukuModal"
+                                            data-id="{{ $book->id }}">Hapus</button>
+                                    </div>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -51,7 +60,7 @@
     </div>
 
     {{-- @endsection --}}
-    
+
     <div class="modal fade" id="tambahBukuModal" tabindex="-1" aria-labelledby="exampleModaLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -93,4 +102,50 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="editBukuModal" tabindex="-1" aria-labelledby="exampleModaLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModaLabel">Edit Data Buku</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('admin.book.submit') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+                        <div class="form-group">
+                            <label for="edit-judul">Judul Buku</label>
+                            <input type="text" class="form-control" name="judul" id="edit-judul" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="edit-penulis">Penulis</label>
+                            <input type="text" class="form-control" name="penulis" id="edit-penulis" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="edit-tahun">Tahun</label>
+                            <input type="year" class="form-control" name="tahun" id="edit-tahun" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="edit-penerbit">Penerbit</label>
+                            <input type="text" class="form-control" name="penerbit" id="edit-penerbit" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="edit-cover">Cover</label>
+                            <input type="file" class="form-control" name="cover" id="edit-cover" required>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="id" id="edit-id">
+                    <input type="hidden" name="old_cover" id="edit-old-cover">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 @stop
--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('admin/books', [App\Http\Controllers\AdminController::class, 'books']
     ->name('admin.books')
     ->middleware('is_admin');
 
-Route::post('amin/books', [App\Http\Controllers\AdminController::class, 'submit_book'])
+Route::post('admin/books', [App\Http\Controllers\AdminController::class, 'submit_book'])
     ->name('admin.book.submit')
     ->middleware('is_admin');
 
