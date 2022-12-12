8e09d5feaa44d965186ccc42d20c0b6c696de884
Ferry Aditya H <ferryaditya124@gmail.com>
2022-12-12 13:13:53 +0700

    #modul 9 bug API

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\Authcontroller;
-
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\API\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,8 +16,10 @@ use App\Http\Controllers\API\Authcontroller;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/books', [BookController::class, 'books']);
+    Route::post('/book/create', [BookController::class, 'create']);
+    Route::post('/book/update/{id}', [BookController::class, 'update']);
+    Route::post('/book/delete/{id}', [BookController::class, 'delete']);
 });
-
-Route::middleware('auth:sanctum')->
+Route::post('/login', [AuthController::class, 'login']);
